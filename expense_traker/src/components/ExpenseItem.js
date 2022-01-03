eac257e28df9b35de2342702ea5fa0cb10224ee3
ashishkumaryad50 <ashishkumaryad50@gmail.com>
2022-01-04 02:34:02 +0530

    2nd commit

--- expense_traker/src/components/ExpenseItem.js
+++ expense_traker/src/components/ExpenseItem.js
@@ -1,20 +1,16 @@
-import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
 
-const ExpenseItem = () => {
-  const expenseDate = new Date(2020, 3, 12);
-  const expenseTitle = 'Room Rent ';
-  const expenseAmount = 300.00;
-    return (
-        <>
-            <div  className="expense-item">
-              <div>{expenseDate.toISOString()}</div>
-              <div className="expense-item__description">
-                <h2>{expenseTitle}</h2>
-                <div className="expense-item__price">${expenseAmount}</div>
-              </div>
-            </div>
-        </>
-    );
+function ExpenseItem(props) {
+  return (
+    <div className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
